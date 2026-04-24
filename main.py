2f81f01829b245238b7cfa1df83a47de54a50418
Shiva <Shivakumar.A@vmaxindia.com>
2026-04-24 16:53:52 +0530

    Update default port to 8008

--- main.py
+++ main.py
@@ -401,4 +401,4 @@ async def monthly_panchang(req: MonthlyPanchangRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8008, reload=True)
